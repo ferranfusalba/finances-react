62b56463003e8465b1979649f94377dcd0afa7cd
Ferran <ferran@protonmail.ch>
2023-02-05 18:33:18 +0100

    Move data to own component

---
+++ src/features/Data/pages/Data.jsx
@@ -0,0 +1,12 @@
+import DataPie from "../ui/DataPie";
+
+const Data = () => {
+  return (
+    <>
+      <p>Data</p>
+      <DataPie width={375} height={375} />
+    </>
+  );
+};
+
+export default Data;
