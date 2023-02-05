3e128fa71beaf21cf8640c1242a605f7c381cbf7
Ferran <ferran@protonmail.ch>
2023-02-05 18:34:00 +0100

    Add redux results to accounts page

--- src/features/Accounts/pages/Accounts.jsx
+++ src/features/Accounts/pages/Accounts.jsx
@@ -1,16 +1,25 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { initializeAccounts } from "../domain/reducers/accountsReducer";
 
 const Accounts = () => {
   const dispatch = useDispatch();
+  const accounts = useSelector((state) => state.accounts);
+
   useEffect(() => {
     dispatch(initializeAccounts());
   }, [dispatch]);
 
   return (
     <>
-      <p>Accounts (from Component)</p>
+      <p>Accounts</p>
+      <ul>
+        {accounts.map((account, index) => (
+          <div key={index}>
+            <li>{account.name}</li>
+          </div>
+        ))}
+      </ul>
     </>
   );
 };
